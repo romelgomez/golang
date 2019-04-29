c4f11d1928b319d4cba0db59149f95c38d6e192f
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2019-04-28 19:56:59 -0500

    Install Go Tools

---
+++ README.md
@@ -0,0 +1,13 @@
+# Go Learning
+
+## Install Go Tools
+
+- Download a binary release suitable for your system:
+
+    https://golang.org/dl/
+
+- Run the installer
+
+    `source bash_scripts/install_go.sh -b go_binary.tar.gz`
+
+- To update-change the version, change the value of `-b` property which it is passed to the installer, `go_binary.tar.gz` and run again.
---
+++ bash_scripts/install_go.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Install Golang Tools
+# ....................
+# TODO:
+# [] Download the last version from:  https://golang.org/dl/
+# [] Make TEST
+
+# Parameters:
+# -b go_binary.tar.gz
+
+function err() { 1>&2 echo "$0: error $@"; return 1; }
+
+function parameters () {
+  while getopts "b:" opt;
+  do
+    case $opt in
+      b) BINARY="$OPTARG" ;;
+      :) err "Option -$OPTARG requires an argument.";;
+      \?) err "Invalid option: -$OPTARG";;
+    esac
+  done
+}
+
+function unpack() {
+  rm -rf $HOME/go/tools/go
+  tar -C $HOME/go/tools -xzvf $BINARY
+}
+
+function env () {
+  if [[ -z "$GOROOT" ]]; then
+      export GOROOT=$HOME/go/tools/go
+      export PATH=$PATH:$GOROOT/bin
+
+      echo '' >> ~/.bashrc
+      echo '# Golang' >> ~/.bashrc
+      echo 'export GOROOT=$HOME/go/tools/go' >> ~/.bashrc
+      echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc    
+      echo '' >> ~/.bashrc
+  fi
+}
+
+function go_tool_version () {
+  echo ""
+  echo "The Go tool version installed is:"
+  echo "------------------------------"  
+  go version
+  echo "------------------------------"  
+  echo ""  
+}
+
+if [ -z "$1" ]; then
+  echo "The option: -b go_binary.tar.gz is missing!"
+else
+  parameters
+  unpack
+  env
+  go_tool_version
+fi
