047fa22e1428bbdb25e22e37dd6aa337b5beded0
Romel Javier Gomez Herrera <bmxandcode@gmail.com>
2019-09-08 22:33:37 -0500

    updating hello word

--- src/github.com/romelgomez/hello/hello.go
+++ src/github.com/romelgomez/hello/hello.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func askNane() {
+	var name string
+	fmt.Println("What is your name?")
+	fmt.Scanf("%s", &name)
+	fmt.Printf("Fuck you %s! \n", name)
+}
+
 func main() {
-	fmt.Printf("hello, world\n")
+	// fmt.Printf("hello, world\n")
+
+	askNane()
 }
